01e2db37fe830b918e408154a0c7639ba3d2505e
Nuelo <akalo.emmanuel18@gmail.com>
2024-09-30 12:00:10 +0100

    feat: create first version meme gen app

---
+++ src/components/Footer.js
@@ -0,0 +1,17 @@
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <small>
+        Cooked by <a
+          className="footer--link"
+          href="https://github.com/nuelose"
+          target="_blank"
+          rel="noreferrer"
+        > 
+          nuelo 
+        </a> <br/>
+        Challenged by Web3Bridge.
+      </small>
+    </footer>
+  );
+}
